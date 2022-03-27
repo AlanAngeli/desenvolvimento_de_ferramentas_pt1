e3e87d96f6ba9f205f61b04cad725f9ac34edd77
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-27 19:57:38 -0300

    Add files via upload

---
+++ comparador_de_hashes.py
@@ -0,0 +1,27 @@
+"""
+Biblioteca Hashlib - Implementa uma interface comum para muitos algoritmos de hash seguro
+como SHA1, SHA256, MD5 entre outros.
+
+https://docs.python.org
+"""
+
+import hashlib
+
+arquivo1 = 'a.txt'
+arquivo2 = 'b.txt'
+
+hash1 = hashlib.new('ripemd160')
+hash1.update(open(arquivo1, 'rb').read())
+
+hash2 = hashlib.new('ripemd160')
+hash2.update(open(arquivo2, 'rb').read())
+
+if hash1.digest() != hash2.digest():
+    #digest resume os dados passados para o update
+    print(f'O arquivo: {arquivo1} é diferente do arquivo: {arquivo2}')
+    print(f'O arquivo: {arquivo1} é: {hash1.hexdigest()}')
+    print(f'O arquivo: {arquivo2} é: {hash2.hexdigest()}')
+else:
+    print(f'O arquivo: {arquivo1} é igual ao arquivo: {arquivo2}')
+    print(f'O arquivo: {arquivo1} é: {hash1.hexdigest()}')
+    print(f'O arquivo: {arquivo2} é: {hash2.hexdigest()}')
